0e0d7d614e3656c5c6428db1deab74c926bffba0
a-a-c-dev <adi.a.c.dev@gmail.com>
2021-08-22 17:39:11 +0300

    Updates

---
+++ precache-manifest.a4c35a3ef5fa18626fb36da620f45215.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66856f7e37825dc0806de0485fd23bc7",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "d8b67aed86047fe400bb",
+    "url": "/to-do-list/static/css/main.5aa0be56.chunk.css"
+  },
+  {
+    "revision": "8383e08d1ed5915bc75a",
+    "url": "/to-do-list/static/js/2.a2d5c703.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/to-do-list/static/js/2.a2d5c703.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8b67aed86047fe400bb",
+    "url": "/to-do-list/static/js/main.d8093513.chunk.js"
+  },
+  {
+    "revision": "93da194e0734e15c4cc8",
+    "url": "/to-do-list/static/js/runtime-main.d859ccf5.js"
+  },
+  {
+    "revision": "1d0950cca7c26a7f448eb2f7c8a79538",
+    "url": "/to-do-list/static/media/add-icon.1d0950cc.svg"
+  },
+  {
+    "revision": "c00baf70df43fe111508b13af5ee2815",
+    "url": "/to-do-list/static/media/bg-image.c00baf70.jpg"
+  },
+  {
+    "revision": "333c5a07597934b1515c4962717fe144",
+    "url": "/to-do-list/static/media/complete-icon.333c5a07.svg"
+  },
+  {
+    "revision": "a6029d89ae840aabdd190b830a3ae9fc",
+    "url": "/to-do-list/static/media/delete-icon.a6029d89.svg"
+  }
+]);
