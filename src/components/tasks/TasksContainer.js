c49dfbbdf610b337a19f25531a0c31e0881fb666
a-a-c-dev <adi.a.c.dev@gmail.com>
2021-08-26 19:48:57 +0300

    work in progress

--- src/components/tasks/TasksContainer.js
+++ src/components/tasks/TasksContainer.js
@@ -71,7 +71,7 @@ const TasksContainer = () => {
                             (
                               <p className="error">
                                 * the list of tasks is full!<br/>
-                                the list of tasks can hold up to ten tasks,<br/>
+                                the list can hold up to ten tasks,<br/>
                                 to add  more tasks
                                 please delete some of the tasks that have been completed 
                               </p>
